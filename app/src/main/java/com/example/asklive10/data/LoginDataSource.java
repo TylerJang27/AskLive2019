f2f19a01961b43324155211957a7dfc62e99a33e
Tyler Jang <taj26@duke.edu>
2019-11-03 13:08:23 -0500

    login credentials

--- app/src/main/java/com/example/asklive10/data/LoginDataSource.java
+++ app/src/main/java/com/example/asklive10/data/LoginDataSource.java
@@ -1,5 +1,7 @@
 package com.example.asklive10.data;
 
+import android.util.Log;
+
 import com.example.asklive10.data.model.LoggedInUser;
 
 import java.io.IOException;
@@ -22,27 +24,37 @@ public class LoginDataSource {
                     new LoggedInUser("Jane Doe", "0001", "example@example.com", "password");
                     //implement login credentials, store userID
 
-            if (email.equals(fakeUser.getEmail()) && password.equals(fakeUser.getPassword())) {
-                boolean student = true;
+            //OLD IMPLEMENTATION if (email.equals(fakeUser.getEmail()) && password.equals(fakeUser.getPassword())) {
+                //OLD IMPLEMENTATIONboolean student = true;
 
                 /*
                 public Student(ArrayList<Question> myStrikeList,
                 String myUserID, String myEmail, String myName, String myPassHash) {}
                  */
                 //TODO: LOAD INFORMATION FROM DATABASE
-                ArrayList<Question> myStrikeList = new ArrayList<Question>();
-                String myUserID = fakeUser.getUserId();
-                String myEmail = fakeUser.getEmail();
-                String myName = fakeUser.getDisplayName();
-                String myPass = fakeUser.getPassword();
-                //TODO: ADD INSTRUCTOR OPTION
-                Globals.setUser(new Student (myStrikeList, myUserID, myEmail, myName, myPass));
+                Hardcode.init();
+                ArrayList<ArrayList<String>> userList = Hardcode.users;
+                Log.i("login", "aaaa" + userList.size() + "pleaseee");
+                for (ArrayList<String> user: userList) {
+                    Log.i("login", "aaaa" + user.get(1));
+                    if (user.get(2).equals(email)) {
+                        Log.i("login", "aaaa" + user.get(1) + "email");
+                        if (user.get(3).equals(password)) {
+                            Log.i("login", "aaaa" + user.get(1) + "pass");
+                            ArrayList<Question> myStrikeList = new ArrayList<Question>();
+                            String myUserID = user.get(0);
+                            String myEmail = user.get(2);
+                            String myName = user.get(1);
+                            String myPass = user.get(3);
+                            //TODO: ADD INSTRUCTOR OPTION
+                            Globals.setUser(new Student (myStrikeList, myUserID, myEmail, myName, myPass));
 
-
-                return new Result.Success<>(fakeUser);
-            } else {
+                            return new Result.Success<>(fakeUser);
+                        }
+                        return new Result.Error(new IOException("Incorrect Login Credentials"));
+                    }
+                }
                 return new Result.Error(new IOException("Incorrect Login Credentials"));
-            }
 
         } catch (Exception e) {
             return new Result.Error(new IOException("Error logging in", e));
