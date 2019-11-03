88a5938ef3f45670ea78fca6f0b377d964908773
Andrew Krier <andrew.krier@duke.edu>
2019-11-03 13:05:03 -0500

    addQ xtml looks better

--- app/src/main/java/com/example/asklive10/AddQuestion.java
+++ app/src/main/java/com/example/asklive10/AddQuestion.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.asklive10.R;
 import com.example.asklive10.classes.Globals;
+import com.example.asklive10.classes.Hardcode;
 import com.example.asklive10.classes.Question;
 
 public class AddQuestion extends AppCompatActivity {
@@ -26,5 +29,18 @@ public class AddQuestion extends AppCompatActivity {
         Question q = new Question(message, Globals.getUser().getUserID(), "00007"); // Post ID tbd
         Globals.setQuestion(q);
         startActivity(i);
+    }
+
+
+    public void onClick(View view) {
+        Log.i("onClick", "hello my guy bleh1");
+        final EditText class_code_input = findViewById(R.id.editText);
+        final Button submitButton = findViewById(R.id.submit);
+        Question ques = new Question(class_code_input.getText().toString(), Globals.getUser().getUserID(), "00007");
+        //Globals.setQuestion(ques);
+        Hardcode.addQuestion(ques, Globals.getGroup().getGroupID());
+        Intent i = new Intent(this, Session.class);
+        startActivity(i);
+        //handle error
     }
 }
